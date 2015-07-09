8761571123c042e2df782ea8380c6264f1797d41
gaocong <gaocong@whut.edu.cn>
2015-07-09 21:28:19 +0800

    框架

---
+++ requestHandlers.js
@@ -0,0 +1,43 @@
+﻿var exec = require("child_process").exec;
+function start(response){
+	console.log("Request handler 'start' was called.");
+	
+	var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html;'+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<textarea name="text" row="20" cols="60"></textarea>'+
+		'<input type="submit" value="提交" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write(body);
+		response.end();
+	// exec("dir d:",{timeout: 10000, maxBuffer:20000*1024},
+		// function(error, stdout, stderr){
+		// response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+		// response.write(stdout);
+		// response.end();
+	// });
+	// function sleep(milliSeconds){
+		// var startTime = new Date().getTime();
+		// while(new Date().getTime() < startTime+milliSeconds);
+	// }
+	
+	// sleep(10000);
+	//return "Hello Start.";
+}
+
+function upload(response){
+	console.log("Request handler 'upload' was called.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	respose.end();
+}
+
+exports.start = start;
+exports.upload = upload;
---
+++ server.js
@@ -0,0 +1,26 @@
+ var http = require("http");
+ var url = require("url");
+ 
+ function start(route, handle){
+	 function onRequest(request, response){
+		 var postData = "";
+		 var pathname = url.parse(request.url).pathname;
+		 console.log("Request for"+pathname+" received.");
+		 request.setEncoding("utf8");
+		 request.addEventListener("data", function(postDataChunk){
+			 postData += postDataChunk;
+			 console.log("Received POST data chunk '" +
+			 postDataChunk + "'");
+		 });
+		 request.addEventListener("end", function(){
+			 route(handle,pathname,response,postData);
+		 })
+		 route(handle, pathname, response);
+	}
+ 
+	http.createServer(onRequest).listen(8888);
+ 	console.log("Server has started."); 
+	
+ }
+ 
+ exports.start = start;
